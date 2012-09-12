de2cb2d1c36ea52b4e379883ba425eabf36ab459
cifweb <cif.web.tech@gmail.com>
2012-09-12 16:42:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = 'c223a5b218ef77380a9eda52b185a3591f5f87460bf61ae53d8e2baae35a9e2cd8882c90e3316d7e287d11a41da8f34e27068f7263441967b69bfca20659d3bb'
